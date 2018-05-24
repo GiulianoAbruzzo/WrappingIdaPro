b4e189cd64913b0a393d452dd1b7e1162dd68300
GiulianoAbruzzo <39120917+GiulianoAbruzzo@users.noreply.github.com>
2018-05-24 11:43:18 +0200

    Add files via upload

--- startServer.py
+++ startServer.py
@@ -1,59 +1,71 @@
 import os
 import subprocess
-from flask import Flask, flash, request, redirect, url_for
-from flask import send_from_directory
+from flask import Flask, request
 from werkzeug.utils import secure_filename
 import json
 
-#modifica la path di dove saranno caricati i file
+#INSERISCI QUI LA PATH DOVE VERRANNO UPLOADATI 
+#I FILE DEL SERVER E DOVE DEVI INSERIRE GLI SCRIPT "script_crea_grafi.py" e "script_crea_lista_funzioni.py"
 UPLOAD_FOLDER = 'C:\Users\Giuliano\Desktop\UploadingFiles'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-@app.route('/estraiFunzioni', methods=['GET', 'POST'])
+    
+@app.route('/estraiFunzioni', methods=['POST'])
 def crea_funzioni():
-    if request.method == 'POST':
-        #salvo il file per poter avviare lo script
-        file = request.files['file']
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    #salvo il file per poter avviare lo script
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         
-        print "Richiesto dizionario funzioni"
-        #subprocess.check_call(['idat64.exe','-A', '-OIDAPython:script_crea_lista_funzioni.py', UPLOAD_FOLDER+'\\'+filename])
-        subprocess.check_call(['ida64.exe','-A', '-OIDAPython:script_crea_lista_funzioni.py', UPLOAD_FOLDER+'\\'+filename])
+    #mi sposto nella cartella di upload e salvo la vecchia cartella di lavoro
+    currentpath=os.getcwd()
+    os.chdir(UPLOAD_FOLDER)
         
-        #elimino i database generati
-        elementi = os.listdir(UPLOAD_FOLDER)
-        for item in elementi:
-                if item.endswith(".i64"):
-                        os.remove(os.path.join(UPLOAD_FOLDER, item))
+    #chiamo lo script "script_crea_lista_funzioni.py"
+    print "Richiesto dizionario funzioni"
+    subprocess.check_call(['idat64.exe','-A','-OIDAPython:script_crea_lista_funzioni.py', UPLOAD_FOLDER+'\\'+filename])
+        
+    #elimino i database generati
+    elementi = os.listdir(UPLOAD_FOLDER)
+    for item in elementi:
+        if item.endswith(".i64"):
+            os.remove(os.path.join(UPLOAD_FOLDER, item))
                         
-        #elimino il file passato
-        os.remove(os.path.join(UPLOAD_FOLDER, filename))                
-        return estrai_funzioni(filename,UPLOAD_FOLDER+'\\')
+    #elimino il file passato
+    os.remove(os.path.join(UPLOAD_FOLDER, filename))  
 
-@app.route('/estraiCFG', methods=['GET', 'POST'])
+    #torno nella cartella del server e chiamo la funzione estrai_funzioni che mi ritorna un dizionario json
+    os.chdir(currentpath)    
+    return estrai_funzioni(filename,UPLOAD_FOLDER+'\\')
+
+@app.route('/estraiCFG', methods=['POST'])
 def crea_grafi():
-    if request.method == 'POST':
-        #salvo il file per poter avviare lo script
-        file = request.files['file']
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    #salvo il file per poter avviare lo script
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-        print "Richiesto dizionario grafi"
-        #subprocess.check_call(['idat64.exe','-A', '-OIDAPython:script_crea_grafi.py', UPLOAD_FOLDER+'\\'+filename])
-        subprocess.check_call(['ida64.exe','-A', '-OIDAPython:script_crea_grafi.py', UPLOAD_FOLDER+'\\'+filename])
+    #mi sposto nella cartella di upload e salvo la vecchia cartella di lavoro
+    currentpath=os.getcwd()
+    os.chdir(UPLOAD_FOLDER)
+         
+    #chiamo lo script "script_crea_grafi.py"
+    print "Richiesto dizionario grafi"
+    subprocess.check_call(['idat64.exe','-A','-OIDAPython:script_crea_grafi.py', UPLOAD_FOLDER+'\\'+filename])
         
-        #elimino i database generati
-        elementi = os.listdir(UPLOAD_FOLDER)
-        for item in elementi:
-                if item.endswith(".i64"):
-                        os.remove(os.path.join(UPLOAD_FOLDER, item))
+    #elimino i database generati
+    elementi = os.listdir(UPLOAD_FOLDER)
+    for item in elementi:
+        if item.endswith(".i64"):
+            os.remove(os.path.join(UPLOAD_FOLDER, item))
                 
-        #elimino il file passato
-        os.remove(os.path.join(UPLOAD_FOLDER, filename))              
-        return estrai_cfg(filename,UPLOAD_FOLDER)        
+    #elimino il file passato
+    os.remove(os.path.join(UPLOAD_FOLDER, filename))   
+
+    #torno nella cartella del server e chiamo la funzione estrai_funzioni che mi ritorna un dizionario json
+    os.chdir(currentpath)
+    return estrai_cfg(filename,UPLOAD_FOLDER)        
         
 def estrai_funzioni(input,path):  
     #crea dizionario
@@ -68,11 +80,12 @@ def estrai_funzioni(input,path):
     #elimino il .txt ora inutile
     os.remove(os.path.join(path, "ListaFunzioni_"+input+".txt"))
     
-    #termino
+    #termino ritornando il dizionario gia in json
     print "Creazione dizionario funzioni terminato"
     return diz
             
 def estrai_cfg(input,path):
+    #salvo la path currente e mi sposto nella cartella creata dallo script crea_grafi dove trovero tutti i grafi della funzione
     print "Creazione dizionario grafi in corso..."
     pathPartenza = path
     nuovaCartella= pathPartenza + "\Grafi_" + input
@@ -89,6 +102,8 @@ def estrai_cfg(input,path):
     dizionario = dict()
     for item in newElenco:
         if item.endswith(".txt"):
+            
+            #apro i vari file dei grafi e levo il .txt finale (-4) e salvo i valori dei grafi in un dizionario
             file = open(item, "r")
             dizionario[item[:-4]]=file.read()
             file.close()
@@ -100,7 +115,7 @@ def estrai_cfg(input,path):
     os.chdir(pathPartenza)
     os.rmdir(nuovaCartella)
     
-    #termino
+    #termino ritornando il dizionario in json
     print "Creazione dizionario grafi terminata"
     return json.dumps(dizionario)
 
